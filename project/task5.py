12939ee963a22a9125a7492f3a19e3fed7e962cf
Hussaine Aljazzar <hussaine70@gmail.com>
2023-09-29 22:46:13 +0300

    Qestions 1-5 Added

---
+++ project/task5.py
@@ -0,0 +1,29 @@
+a_Seperator = "---------------------------------------------------------------------------"
+# 01
+print("Question No.01")
+print("Please Enter Your Name ,Age ,street, city, and country")
+print(a_Seperator)
+# 02
+print("Question No.02")
+user_Name = "Lama"
+user_Age = "20"
+user_Street = "159"
+user_city = "Gaza"
+user_Country = "Palestine"
+full_Data = f'"Name: {user_Name}" \n "Age: {user_Age}" \n "Address: {user_Street}, {user_city}, {user_Country}" '
+print(full_Data)
+print(a_Seperator)
+# 03
+print("Question No.03")
+print("# Hello Lama I Modified Your Age Based on the Task question no.3")
+print(
+    f'" Hello {user_Name} Yor Age is {int(user_Age) - 5} Years Old ,Your \n Address is {user_Street}, {user_city}, {user_Country}"')
+print(a_Seperator)
+# 04
+print("Question No.04")
+print(f"{type(user_Name)}, {type(user_Age)}, \n {type(user_Street)}, {type(user_city)},\n {type(user_Country)} ")
+print(a_Seperator)
+# 05
+print("Question No.05")
+print(f' "Hello {user_Name}, How Are You \\ ""\" Your Age is "{user_Age}"" + \n And Your Country is {user_Country} ')
+print(a_Seperator)
