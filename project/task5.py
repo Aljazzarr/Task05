28da04a2a98d3759fed4cd9ca785d533a90571f2
Hussaine Aljazzar <hussaine70@gmail.com>
2023-09-29 23:02:56 +0300

    Questions 11-19 Added

--- project/task5.py
+++ project/task5.py
@@ -56,3 +56,64 @@ msg = "I %7 Python And Although I %7 GSG with Trainer Eng-Zakaria"
 msg = msg.replace("%7", "love")
 print(msg)
 print(a_Seperator)
+# 11 + 12
+print("Question No.11+12")
+num1 = "4"
+num2 = "56"
+num3 = "963"
+num4 = "385"
+num5 = "8719"
+num6 = "87190"
+print(num1.zfill(5))
+print(num2.zfill(5))
+print(num3.zfill(5))
+print(num4.zfill(5))
+print(num5.zfill(5))
+print(num6.zfill(5))
+print(a_Seperator)
+# 13
+print("Question No.13")  # two examples
+my_String = "i love python with eng zakarya "
+print(my_String.capitalize())
+print("# Capitalize makes the first letter in the string to uppercase")
+print(my_String.title())
+print("# Title makes the first letter in Every word in string to uppercase")
+first_name = "Hussaine"
+print(a_Seperator)
+# 14
+print("Question No.14")
+print("*" * 10 + first_name)
+print("*" * 10 + first_name + "*" * 11)
+print(first_name + "*" * 11)
+print(a_Seperator)
+# 15
+print("Question No.15")
+name_one = "SaMER"
+name_two = "Abed"
+print(name_one.swapcase())
+print(name_two.swapcase())
+print(name_one.lower())
+print(name_two.upper())
+print(a_Seperator)
+# 16
+print("Question No.16")
+print(name_one.isupper())
+print(name_two.islower())
+print(a_Seperator)
+#17
+print("Question No.17")
+print(name_one.startswith("S"))
+print(name_two.endswith("HD"))
+print(a_Seperator)
+# 18
+print("Question No.18")
+msg = "I Love Python And Although I Love GSG with Eng. Zakaria"
+print(f'Number of <Love> is {msg.count("Love")}')
+print(f'Number of <t> is {msg.count("t")}')
+print(a_Seperator)
+# 19
+print("Question No.19")
+msg = "I %7 Python And Although I %7 GSG with Zakaria"
+msg = msg.replace("%7", "love", 1)
+print(msg)
+print(a_Seperator)
